33476952ffde114150ed45d9cdf4b280958b8507
e33io <info@e33.io>
2025-06-06 22:41:15 -0500

    Add font files

---
+++ README.md
@@ -0,0 +1,23 @@
+# adwaita-mono-nf
+
+AdwaitaMono Nerd Font
+
+&nbsp;
+
+[AdwaitaMono](https://gitlab.gnome.org/GNOME/adwaita-fonts/-/tree/main/mono) with additional [Nerd Font](https://www.nerdfonts.com) patched versions
+- [AdwaitaMono](https://github.com/e33io/adwaita-mono-nf/tree/main/AdwaitaMono/AdwaitaMono)
+- [AdwaitaMono Nerd Font](https://github.com/e33io/adwaita-mono-nf/tree/main/AdwaitaMono/AdwaitaMono-Nerd)
+- [AdwaitaMono Nerd Font Mono](https://github.com/e33io/adwaita-mono-nf/tree/main/AdwaitaMono/AdwaitaMono-Nerd-Mono)
+- [AdwaitaMono Nerd Font Propo](https://github.com/e33io/adwaita-mono-nf/tree/main/AdwaitaMono/AdwaitaMono-Nerd-Propo)
+
+&nbsp;
+
+### Install (Linux)
+```
+curl -s https://raw.githubusercontent.com/e33io/adwaita-mono-nf/refs/heads/main/install.sh | sh
+```
+
+&nbsp;
+
+### License
+[SIL OPEN FONT LICENSE](https://github.com/e33io/adwaita-mono-nf/blob/main/LICENSE)
---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# ==================================================================
+# Install AdwaitaMono Nerd Fonts
+# URL: https://github.com/e33io/adwaita-mono-nf/blob/main/install.sh
+# ==================================================================
+
+if [ ! -f "/usr/bin/git" ]; then
+    echo "#########################################################"
+    echo "This script requires git. Install git and try again."
+    echo "#########################################################"
+    exit 1
+fi
+
+echo "#########################################################"
+echo "Clone font files and make truetype directory"
+echo "#########################################################"
+
+git clone https://github.com/e33io/adwaita-mono-nf $HOME/temp-fonts
+sudo mkdir -p /usr/share/fonts/truetype
+
+echo "#########################################################"
+echo "Copy font files to the truetype directory"
+echo "#########################################################"
+
+sudo cp -R $HOME/temp-fonts/AdwaitaMono/AdwaitaMono /usr/share/fonts/truetype
+sudo cp -R $HOME/temp-fonts/AdwaitaMono/AdwaitaMono-Nerd /usr/share/fonts/truetype
+sudo cp -R $HOME/temp-fonts/AdwaitaMono/AdwaitaMono-Nerd-Mono /usr/share/fonts/truetype
+sudo cp -R $HOME/temp-fonts/AdwaitaMono/AdwaitaMono-Nerd-Propo /usr/share/fonts/truetype
+
+echo "#########################################################"
+echo "Clean up temp files and cache fonts"
+echo "#########################################################"
+
+sudo rm -Rf $HOME/temp-fonts
+fc-cache -f
+
+echo "#########################################################"
+echo "AdwaitaMono Nerd Fonts are now installed"
+echo "#########################################################"
